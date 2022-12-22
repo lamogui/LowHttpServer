1d5bf9dc185d7935ff4c8b1dbf5da949d5f97a81
lamogui <julien.deloor@gmail.com>
2022-12-22 17:29:56 +0100

    Added file log for debug service + 1 thread per request

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,102 @@
+
+#![allow(unused_parens)]
+#![allow(unused_labels)]
+
+extern crate low;
+use low::Log;
+use low::LogWarning;
+use low::LogError;
+
+
 use std::env;
+
 use std::io::BufReader;
 use std::io::Read;
-use std::net::{TcpListener, TcpStream};
-use std::fs::File;
-use std::mem::transmute;
-use std::collections::HashMap;
 use std::io::Write;
+
+use std::net::TcpListener;
+use std::net::TcpStream;
+
+use std::fs::File;
+
+use std::collections::HashMap;
+
+use std::sync::Arc;
+
+fn HandleConnection( mut _stream : TcpStream, _fileEntries: &HashMap< String, Vec<u8> > ) {
+	let mut requestBuffer: Vec< u8 > = Vec::new();
+				
+	let socketStr: String= match _stream.peer_addr() {
+		Ok( a ) => {
+			a.to_string()
+		} Err( e ) => {
+			low::Error!( "Impossible de recuperer l'addresse {:?}", e );
+			"<erreur>".to_string()
+		}
+	};
+	'singleRequestLoop: loop {
+		let mut tempBuffer: [u8; 1024] = [0; 1024]; 
+		match _stream.read( &mut tempBuffer ) {
+			Ok( size ) => {
+				if (size > 0) {
+					requestBuffer.extend_from_slice( &tempBuffer );
+					let requestString: String = match String::from_utf8( requestBuffer.clone() ) {
+						Ok( s ) => { 
+							s 
+						} Err( e ) => { 
+							low::Error!( "Utf8 error {} {:?}", socketStr.as_str(), e );
+							break 'singleRequestLoop;
+						}
+					};
+
+					if ( requestString.len() > 9000 ) {
+						low::Error!( "Huge input request {} {}", socketStr.as_str(), requestString.as_str() );
+						break 'singleRequestLoop; // break connection if the request is too long
+					}
+
+					// detect empty line that mean request is finnished 
+					if ( requestString.find( "\r\n\r\n" ).is_some() || requestString.find( "\n\n" ).is_some() ) {
+						let mut split = requestString.split_whitespace();
+						split.next(); // skip GET/POST etc
+						match ( split.next() ) {
+							Some( uri ) => {
+								//let uriOwned = uri.to_string();
+								//let uriSlash = uri.to_string() + "/";
+								let possibleUris: [ &str; 1] = [ uri ];
+								for uri in possibleUris.into_iter() {
+									match ( _fileEntries.get_key_value( uri ) ) {
+										Some( (key, value) ) => {
+											match _stream.write_all( &value ) {
+												Ok(_) => {
+													low::Printf!( "Send page {} success for {}\n", key.as_str(), socketStr.as_str() );
+													break 'singleRequestLoop;
+												}
+												Err( e ) => {
+													low::Error!( "Failed to send page {} for {} {}", key.as_str(), socketStr.as_str(), e );
+													break 'singleRequestLoop;
+												}
+											}
+										} None => {
+											low::Warning!( "No 404 page for request {} from {}", uri, socketStr.as_str() );
+											break 'singleRequestLoop; // todo 
+										}
+									} 
+								}
+							} None => {
+								// continue until having more characters
+							}
+						}
+					}
+				}
+			} Err( e ) => {
+				low::Error!( "Erreur de lecture de socket {} {:?}", socketStr.as_str(), e );
+				break 'singleRequestLoop;
+			}
+		}
+	}
+}
+
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -24,7 +115,7 @@ fn main() {
                 f
             }
             Err(e) => {
-                println!("Erreur impossible d'ouvrir {} : {:?}", packFilename.as_str(), e);
+                low::Error!("Erreur impossible d'ouvrir {} : {:?}", packFilename.as_str(), e);
                 return;
             }
         };
@@ -33,142 +124,74 @@ fn main() {
         'readFile: loop {
 			let mut keySizeBuf : [u8;4] = [0,0,0,0];
             match reader.read_exact(&mut keySizeBuf) {
-				Ok ( size ) => {
-					//assert_eq!( size, 4 );
+				Ok ( _ ) => {
 				}
-				Err( e ) => {
+				Err( _ ) => {
 					break 'readFile;					
 				}
 			}
-			let keySize : u32 = u2::from_le_bytes( keySizeBuf );
-			let mut keyVec: Vec< u8 > = vec![ 0; keysize ];
+			let keySize : u32 = u32::from_le_bytes( keySizeBuf );
+			let mut keyVec: Vec< u8 > = Vec::new();
+			keyVec.resize( keySize as usize, 0 );
 			match reader.read_exact(&mut keyVec) {
-				Ok ( size ) => {
-					//assert_eq!( size, keySize as usize );
+				Ok ( _ ) => {
 				}
 				Err( e ) => {
-					println!("Erreur impossible de lire la clef {:?}", e);
+					low::Error!("Impossible de lire la clef {:?}", e);
 					break 'readFile;					
 				}
 			}
 			let keyString: String = match String::from_utf8( keyVec ) {
 				Ok( s ) => { s }
 				Err( e ) => { 
-					println!("Erreur impossible de decoder l'utf8 {:?}", e);
+					low::Error!("Impossible de decoder l'utf8 {:?}", e);
 					break 'readFile;	
 				}
 			};
 			let mut answerSizeBuf : [u8;4] = [0,0,0,0];
             match reader.read_exact(&mut answerSizeBuf) {
-				Ok ( size ) => {
-					//assert_eq!( size, 4 );
+				Ok ( _ ) => {
 				}
 				Err( e ) => {
-					println!("Erreur impossible de lire la taille des datas de {} {:?}", keyString.as_str(), e);
+					low::Error!("Impossible de lire la taille des datas de {} {:?}", keyString.as_str(), e);
 					break 'readFile;					
 				}
 			}
-			let mut answerSize : u32 = u32::from_le_bytes( answerSizeBuf );
-			let mut dataVec: Vec< u8 > = vec![ 0; answerSize ];
+			let answerSize : u32 = u32::from_le_bytes( answerSizeBuf );
+			let mut dataVec: Vec< u8 > = Vec::new();
+			dataVec.resize( answerSize as usize, 0 );
 			match reader.read_exact(&mut dataVec) {
-				Ok ( size ) => {
-					//assert_eq!( size, answerSize as usize );
+				Ok ( _ ) => {
 				}
 				Err( e ) => {
-					println!("Erreur durant la lecture des datas de {} {:?}", keyString.as_str(), e);
+					low::Error!("Impossible de lire les datas de {} {:?}", keyString.as_str(), e);
 					break 'readFile;					
 				}
 			}
-			println!( "Readed {} {} bytes", keyString.as_str(), answerSize );
+			low::Printf!( "Readed {} ({} bytes)\n", keyString.as_str(), answerSize );
 			if ( !fileEntries.insert( keyString.clone(), dataVec ).is_none() ) {
-				println!( "Warning, double insertion for key {}", keyString.as_str() );
+				low::Warning!( "Double insertion for key {}", keyString.as_str() );
 			}
         }
     }
 
+	let fileEntriesShared = Arc::new( fileEntries );
+
     let listener: TcpListener = match TcpListener::bind("0.0.0.0:80") {
         Ok( l ) => { l }
         Err(e) => {
-            println!("Erreur de creer le server : {:?}", e);
+            low::Error!("Impossible de binder la socket : {:?}", e);
             return;
         }
     };
 
     'serverLoop: for stream in listener.incoming() {
         match stream {
-            Ok( mut stream) => {
-				let mut requestBuffer: Vec< u8 > = Vec::new();
-				
-				let socketStr: String= match stream.peer_addr() {
-					Ok( a ) => {
-						a.to_string()
-					} Err( e ) => {
-						println!( "Erreur impossible de recuperer l'addresse {:?}", e );
-						"<erreur>".to_string()
-					}
-				};
-				'singleRequestLoop: loop {
-					let mut tempBuffer: [u8; 1024] = [0; 1024]; 
-					match stream.read( &mut tempBuffer ) {
-						Ok( size ) => {
-							if (size > 0) {
-								requestBuffer.extend_from_slice( &tempBuffer );
-								let requestString: String = match String::from_utf8( requestBuffer.clone() ) {
-									Ok( s ) => { 
-										s 
-									} Err( e ) => { 
-										println!( "Utf8 error {} {:?}", socketStr.as_str(), e );
-										break 'singleRequestLoop;
-									}
-								};
-
-								if ( requestString.len() > 9000 ) {
-									println!( "Huge input request {} {}", socketStr.as_str(), requestString.as_str() );
-									break 'singleRequestLoop; // break connection if the request is too long
-								}
-
-								// detect empty line that mean request is finnished 
-								if ( requestString.find( "\r\n\r\n" ).is_some() || requestString.find( "\n\n" ).is_some() ) {
-									let mut split = requestString.split_whitespace();
-									split.next(); // skip GET/POST etc
-									match ( split.next() ) {
-										Some( uri ) => {
-											//let uriOwned = uri.to_string();
-											//let uriSlash = uri.to_string() + "/";
-											let possibleUris: [ &str; 1] = [ uri ];
-											for uri in possibleUris.into_iter() {
-												match ( fileEntries.get_key_value( uri ) ) {
-													Some( (key, value) ) => {
-														match stream.write_all( &value ) {
-															Ok(_) => {
-																println!( "Send page {} success for {}", uri, socketStr.as_str() );
-																break 'singleRequestLoop;
-															}
-															Err( e ) => {
-																println!( "Failed to send page {} for {}", uri, socketStr.as_str() );
-																break 'singleRequestLoop;
-															}
-														}
-													} None => {
-														println!( "No 404 page for request {} from {}", uri, socketStr.as_str() );
-														break 'singleRequestLoop; // todo 
-													}
-												} 
-											}
-										} None => {
-											// continue until having more characters
-										}
-									}
-								}
-							}
-						} Err( e ) => {
-							println!( "Erreur de lecture de socket {} {:?}", socketStr.as_str(), e );
-							break 'singleRequestLoop;
-						}
-					}
-				}
-            }
-            Err(e) => { println!("Erreur connection : {:?}", e); }
-        }
+            Ok( stream) => {
+				let fileEntryClone = fileEntriesShared.clone();
+				std::thread::spawn( move || { HandleConnection( stream, &fileEntryClone ); } );
+			}	
+            Err(e) => { low::Error!("Connection : {:?}", e); }
+		}
     }
 }
